c08c77eae4bd5c9773ce3e1b84c790113f116fef
Adam Goler <gol3tron@pop-os.localdomain>
2024-03-15 22:00:38 -0700

    added performance library for 172S.

---
+++ performance_172S.py
@@ -0,0 +1,61 @@
+import numpy as np
+import scipy as sp
+
+import tables_172S as tb
+
+
+def get_ground_roll_sfto(pressure_altitude: float, weight: float, temperature: float) -> float:
+    """
+    Compute the ground roll for short field takeoff in feet based on weight, pressure altitude and temperature.
+
+    Return ground roll (feet) as a float value.
+    """
+
+    # Compute ground_roll
+    points = (tb.weight_index, tb.temperature_index, tb.pressure_altitude_index)
+    point = np.array([weight, temperature, pressure_altitude])
+    ground_roll = sp.interpolate.interpn(points, tb.sfto_ground_roll, point)[0]
+
+    return ground_roll
+
+def get_dist_50ft_sfto(pressure_altitude: float, weight: float, temperature: float) -> float:
+    """
+    Compute the distance (in feet) at 50 ft height for short field takeoff based on weight, pressure altitude and temperature.
+
+    Return dist_50ft as a float value.
+    """
+
+    # Compute ground_roll
+    points = (tb.weight_index, tb.temperature_index,
+              tb.pressure_altitude_index)
+    point = np.array([weight, temperature, pressure_altitude])
+    dist_50ft = sp.interpolate.interpn(points, tb.sfto_dist_50_feet, point)[0]
+
+    return dist_50ft
+
+def get_lift_off_speed(weight: float) -> float:
+    """
+    Compute the lift off speed in knots for a short field takeoff.
+
+    Return a float value for lift_off_speed.
+    """
+
+    # Compute lift_off_speed
+    lift_off_speed = np.interp(
+        np.array(weight), tb.weight_index[::-1], tb.lift_off_index[::-1])
+
+    return lift_off_speed
+
+
+def get_speed_at_50ft(weight: float) -> float:
+    """
+    Compute the speed at 50 ft height in knots for a short field takeoff.
+
+    Return a float value for speed_at_50ft.
+    """
+
+    # Compute lift_off_speed
+    speed_at_50ft = np.interp(
+        np.array(weight), tb.weight_index[::-1], tb.speed_at_50_feet_index[::-1])
+
+    return speed_at_50ft
